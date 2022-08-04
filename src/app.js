38ada5d5817e3916b5cb1c0cf37eb6b8b030f9c4
Oscar Urrutia <oscarurrutia940@gmail.com>
2022-08-04 14:45:17 +0000

    todo menos modal

--- src/app.js
+++ src/app.js
@@ -6,6 +6,44 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  function carta() {
+    let arrayNumeros = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "Q", "J", "K"];
+
+    let arrayCartas = ["♦", "♥", "♠", "♣"];
+
+    let carta = arrayNumeros[random(arrayNumeros.length)];
+
+    let palo = arrayCartas[random(arrayCartas.length)];
+
+    if (palo == "♦" || palo == "♥")
+      document.querySelector(".top").style.color = "red";
+    else document.querySelector(".top").style.color = "black";
+
+    if (palo == "♦" || palo == "♥")
+      document.querySelector(".button").style.color = "red";
+    else document.querySelector(".button").style.color = "black";
+
+    if (palo == "♦" || palo == "♥")
+      document.querySelector(".number").style.color = "red";
+    else document.querySelector(".number").style.color = "black";
+
+    if (carta == "A") carta = palo;
+
+    document.querySelector(".top").innerHTML = palo;
+    document.querySelector(".number").innerHTML = carta;
+    document.querySelector(".button").innerHTML = palo;
+
+    return;
+  }
+
+  carta();
+
+  function random(maxNumber) {
+    return Math.floor(Math.random() * maxNumber);
+  }
+
+  let botonNext = document.querySelector("#next-card");
+  botonNext.addEventListener("click", function() {
+    carta();
+  });
 };
